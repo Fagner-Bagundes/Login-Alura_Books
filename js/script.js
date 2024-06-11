abc7232cdc2e2daf13bfba6e24c11956f46589fd
Fagner_0 <ferreirabagundes@gmail.com>
2024-06-11 17:42:02 -0300

    projeto concluido

--- js/script.js
+++ js/script.js
@@ -1,3 +1,28 @@
-let consultaCEP = fetch(`https://viacep.com.br/ws/01001000/json/`);
+async function BuscaEndereco(cep) {
+    var mensagemErro = document.getElementById(`erro`);
+    mensagemErro.innerHTML = ``;
+try {
+    let consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
+    let consultaCEPConvertida = await consultaCEP.json();
+    if (consultaCEPConvertida.erro) {
+        throw Error(`CEP não existente`);
+    }
+    let cidade = document.getElementById(`cidade`);
+    let logradouro = document.getElementById(`endereco`)
+    let estado = document.getElementById(`estado`)
 
-console.log(consultaCEP);
+    cidade.value = consultaCEPConvertida.localidade;
+    logradouro.value = consultaCEPConvertida.logradouro;
+    estado.value = consultaCEPConvertida.uf;
+
+    console.log(consultaCEPConvertida);
+    return consultaCEPConvertida;
+} catch (error) {
+    mensagemErro.innerHTML = `<p>CEP inválido. Tente novamente`
+    console.log(error);
+}
+}
+
+
+let cep = document.getElementById(`cep`);
+cep.addEventListener(`focusout`, () => BuscaEndereco(cep.value))
